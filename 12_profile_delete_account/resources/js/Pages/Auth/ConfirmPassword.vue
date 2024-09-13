44b9694f01241ba7a4ddeab122bd3d01b57cac62
JonVadar <vadarjon@gmail.com>
2024-09-13 15:47:43 +0700

    Add delete user account source code

---
+++ 12_profile_delete_account/resources/js/Pages/Auth/ConfirmPassword.vue
@@ -0,0 +1,46 @@
+<script setup>
+import Container from "../../Components/Container.vue";
+import Title from "../../Components/Title.vue";
+import TextLink from "../../Components/TextLink.vue";
+import InputField from "../../Components/InputField.vue";
+import PrimaryBtn from "../../Components/PrimaryBtn.vue";
+import ErrorMessages from "../../Components/ErrorMessages.vue";
+import SessionMessages from "../../Components/SessionMessages.vue";
+import CheckBox from "../../Components/CheckBox.vue";
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    password: "",
+});
+
+const submit = () => {
+    form.post(route("password.confirm"), {
+        onFinish: () => form.reset(),
+    });
+};
+</script>
+
+<template>
+    <Head title="- Password Confirmation" />
+    <Container class="w-1/2">
+        <div class="mb-8 text-center">
+            <p>
+                This is a secure area of the application. Please confirm your
+                password before continuing.
+            </p>
+        </div>
+
+        <ErrorMessages :errors="form.errors" />
+
+        <form @submit.prevent="submit" class="space-y-6">
+            <InputField
+                label="Password"
+                type="password"
+                icon="key"
+                v-model="form.password"
+            />
+
+            <PrimaryBtn :disabled="form.processing">Confirm</PrimaryBtn>
+        </form>
+    </Container>
+</template>
---
+++ 12_profile_delete_account/resources/js/Pages/Profile/Sections/UpdatePassword.vue
@@ -0,0 +1,64 @@
+<script setup>
+import Container from "../../../Components/Container.vue";
+import Title from "../../../Components/Title.vue";
+import InputField from "../../../Components/InputField.vue";
+import PrimaryBtn from "../../../Components/PrimaryBtn.vue";
+import ErrorMessages from "../../../Components/ErrorMessages.vue";
+import SessionMessages from "../../../Components/SessionMessages.vue";
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    current_password: "",
+    password: "",
+    password_confirmation: "",
+});
+
+const submit = () => {
+    form.put(route("profile.password"), {
+        onSuccess: () => form.reset(),
+        onError: () => form.reset(),
+        preserveScroll: true,
+    });
+};
+</script>
+
+<template>
+    <Container class="mb-6">
+        <div class="mb-6">
+            <Title>Update Password</Title>
+            <p>Ensure your are using a long, random password to stay secure.</p>
+        </div>
+
+        <ErrorMessages :errors="form.errors" />
+
+        <form @submit.prevent="submit" class="space-y-6">
+            <InputField
+                label="Current Password"
+                icon="key"
+                class="w-1/2"
+                type="password"
+                v-model="form.current_password"
+            />
+
+            <InputField
+                label="New Password"
+                icon="key"
+                class="w-1/2"
+                type="password"
+                v-model="form.password"
+            />
+
+            <InputField
+                label="Confirm New Password"
+                icon="key"
+                class="w-1/2"
+                type="password"
+                v-model="form.password_confirmation"
+            />
+
+            <p v-if="form.recentlySuccessful" class="text-green-500 font-medium">Saved!</p>
+
+            <PrimaryBtn :disabled="form.processing">Save</PrimaryBtn>
+        </form>
+    </Container>
+</template>
